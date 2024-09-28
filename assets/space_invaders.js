9b183771919a9f5a670fa35177a29cd5a51ca89d
Trentlane1 <trentlane1987@hotmail.com>
2024-09-28 09:56:10 -0500

    Create space_invaders.js

---
+++ assets/space_invaders.js
@@ -0,0 +1,5 @@
+// Basic Space Invaders game logic will go here
+const canvas = document.getElementById("spaceInvadersCanvas");
+const ctx = canvas.getContext("2d");
+
+// Your Space Invaders game logic goes here...
